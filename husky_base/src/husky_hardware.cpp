4705f1dee73f7290f3351d01fd2af59eca3f09b8
Matěj Boxan <matej.boxan@gmail.cz>
2023-03-08 10:53:21 -0500

    Fix compilation with hardware_interface installed with rosdep

--- husky_base/src/husky_hardware.cpp
+++ husky_base/src/husky_hardware.cpp
@@ -335,7 +335,7 @@ rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn HuskyH
   return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
 }
 
-hardware_interface::return_type HuskyHardware::read()
+hardware_interface::return_type HuskyHardware::read(const rclcpp::Time & time, const rclcpp::Duration & period)
 {
   RCLCPP_DEBUG(rclcpp::get_logger(HW_NAME), "Reading from hardware");
 
@@ -346,7 +346,7 @@ hardware_interface::return_type HuskyHardware::read()
   return hardware_interface::return_type::OK;
 }
 
-hardware_interface::return_type HuskyHardware::write()
+hardware_interface::return_type HuskyHardware::write(const rclcpp::Time & time, const rclcpp::Duration & period)
 {
   RCLCPP_DEBUG(rclcpp::get_logger(HW_NAME), "Writing to hardware");
 
