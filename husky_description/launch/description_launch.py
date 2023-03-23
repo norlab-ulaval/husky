aedc741ddc97701085be8de35e119f33074d5593
Damien LaRocque <phicoltan@gmail.com>
2023-03-23 14:24:31 -0400

    Launch husky description like in marmotte_description

--- husky_description/launch/description_launch.py
+++ husky_description/launch/description_launch.py
@@ -1,30 +1,27 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.descriptions import ParameterValue
 
 
 def generate_launch_description():
     # Get URDF via xacro
-    robot_description_content = Command(
-        [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
-            PathJoinSubstitution(
-                [FindPackageShare("husky_description"), "urdf", "husky.urdf.xacro"]
-            ),
-            " ",
-            "name:=husky",
-            " ",
-            "prefix:=''",
-            " ",
-        ]
-    )
-    robot_description = {"robot_description": robot_description_content}
+    package = get_package_share_directory("husky_description")
+    xacro_path = os.path.join(package, "urdf/husky.urdf.xacro")
+
+    robot_description_command = Command(["xacro ", xacro_path, " name:=husky"])
+    robot_description = {
+        "robot_description": ParameterValue(robot_description_command, value_type=str)
+    }
 
     node_robot_state_publisher = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
+        name="robot_state_publisher",
         output="screen",
         parameters=[robot_description],
     )
