55b7049feb849fa9f30d95887d39089b9cd0c1e2
norlab-husky <norlabhusky@gmail.com>
2023-04-14 10:18:16 -0400

    Launch robot description in base_launch the same way as marmotte_description

--- husky_base/launch/base_launch.py
+++ husky_base/launch/base_launch.py
@@ -1,28 +1,22 @@
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-
+from launch_ros.descriptions import ParameterValue
 import xacro
 
 
 def generate_launch_description():
     # Get URDF via xacro
-    robot_description_content = Command(
-        [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
-            PathJoinSubstitution(
-                [FindPackageShare("husky_description"), "urdf", "husky.urdf.xacro"]
-            ),
-            " ",
-            "name:=husky",
-            " ",
-            "prefix:=''",
-            " ",
-        ]
-    )
-    robot_description = {"robot_description": robot_description_content}
+    # Get URDF via xacro
+    package = get_package_share_directory("husky_description")
+    xacro_path = os.path.join(package, "urdf/husky.urdf.xacro")
+
+    robot_description_command = Command(["xacro ", xacro_path, " name:=husky"])
+    robot_description = {
+        "robot_description": ParameterValue(robot_description_command, value_type=str)
+    }
 
     config_husky_velocity_controller = PathJoinSubstitution(
         [FindPackageShare("husky_control"),
@@ -33,6 +27,7 @@ def generate_launch_description():
     node_robot_state_publisher = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
+        name="robot_state_publisher",
         output="screen",
         parameters=[robot_description],
     )
